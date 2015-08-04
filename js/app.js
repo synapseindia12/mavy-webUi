8c38a1678077eccfe61021e122723b2d55da340a
Rishabh Shukla <rishukla@sampatti.com>
2015-08-04 17:06:01 +0530

    first comit

---
+++ js/app.js
@@ -0,0 +1,272 @@
+var myApp = angular.module("mavyApp", ['ngRoute', 'ngCookies', 'ngStorage']);
+myApp.config(function($routeProvider, $httpProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'signup.html',
+			controller: 'signupCtrl'
+        })
+		.when('/dashboard', {
+            templateUrl: 'home.html',
+			controller: 'indexCtrl'
+        })
+		.when('/assignment', {
+            templateUrl: 'assignments.html',
+			controller: 'assignmentCtrl'
+        })
+		.when('/forum', {
+            templateUrl: 'forum.html',
+			controller: 'forumCtrl'
+        })
+		.when('/messages', {
+            templateUrl: 'messages.html',
+			controller: 'messagesCtrl'
+        })
+		.when('/profile', {
+            templateUrl: 'profile.html',
+			controller: 'profileCtrl'
+        })
+		.when('/notification', {
+            templateUrl: 'notifications.html',
+			controller: 'notificationCtrl'
+        })
+		.when('/resetpassword', {
+            templateUrl: 'resetpassword.html',
+			controller: 'resetpasswordCtrl'
+        })
+		.otherwise({
+            redirectTo: '/'
+        });
+		
+		//$facebookProvider.setAppId('1588022388118943');
+		// $httpProvider.interceptors.push(function($q, $location) { 
+			// return {
+				// response: function(response) {
+					//do something on success return response; 
+				// }, responseError: function(response) {
+					// if (response.status === 401) 
+						// $location.url('/login'); 
+					// return $q.reject(response);
+				// }
+			// }; 
+		// });
+	})
+	.run(function($rootScope){
+		(function(){
+		if (document.getElementById('facebook-jssdk')) {return;}
+		var firstScriptElement = document.getElementsByTagName('script')[0];
+		var facebookJS = document.createElement('script');
+		facebookJS.id = 'facebook-jssdk';
+		facebookJS.src = 'http://connect.facebook.net/en_US/sdk.js';
+		firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+		}());
+	});
+	
+myApp.controller('fbCtrl', function ($scope) {
+
+}); 
+
+myApp.controller('signupCtrl', function($scope, $rootScope, $location, $cookieStore, $localStorage, $window){
+	if ($localStorage.loggedIn) {
+		$location.path('/dashboard');
+	}
+	var endpoints = {};
+	$scope.newArray = [];
+	endpoints.apiKey = "835mzggn289l9wxnjxjr323kny6q";
+	endpoints.mobileHandler = new MobileHandler();
+	
+	$scope.signUp = function() {
+		$scope.userSysteminfo = [];
+		if($scope.uname && $scope.password){
+			endpoints.mobileHandler.login($scope.uname, $scope.password, $scope.userSysteminfo, function(result){
+				if(result){					
+					$scope.uname = '';
+					$scope.password = '';
+					$localStorage.loggedIn = true;
+					$localStorage.loginDetails = result.result.result;
+					$location.path('/dashboard');
+					$scope.$apply();
+				}
+			});
+		}
+	};
+	
+	$scope.fbLogin = function() {
+		$facebook.login().then(function(result) {
+			debugger;
+		});
+	};	
+});
+
+myApp.controller('indexCtrl', function($scope, $cookieStore, $rootScope, $localStorage){
+	if(!$localStorage.loginDetails){
+		delete $localStorage.loggedIn;
+		$location.path('/');
+	}
+	$scope.allAssignments = [];
+	var loginDetails = $localStorage.loginDetails;
+	$scope.apiKey = loginDetails[0].value;
+	$scope.userId = loginDetails[1].value;
+	$scope.panelistId = loginDetails[2].value;
+	$scope.registrationId = loginDetails[3].value;
+	var sectionId = 2;
+	$scope.tempArray = [];
+	$scope.allPolls = [];
+	
+	var endpoints = {};
+	endpoints.apiKey = $scope.apiKey;
+	endpoints.mobileHandler = new MobileHandler();
+	endpoints.mobileHandler.getDashboard($scope.apiKey,$scope.userId,7,null,null, function(result){
+		debugger;
+	});
+	endpoints.mobileHandler.getAssignments($scope.apiKey, $scope.userId, $scope.panelistId, function(result){
+			if (result.result.success){
+				for (var i=0; i< result.result.result.length; i++) {
+					$scope.allAssignments.push(result.result.result[i]);
+				}
+			}
+			$scope.$apply();
+		});
+	endpoints.mobileHandler.getDashboard($scope.apiKey,$scope.userId,2,null,null, function(result){
+		$scope.replyPosts = result.result.result;
+		for(var i=0; i< $scope.replyPosts.Entries.length; i++){
+			$scope.tempArray.push($scope.replyPosts.Entries[i]);
+			$scope.$apply();
+		}
+		endpoints.mobileHandler.getDashboard($scope.apiKey,$scope.userId,3,null,null, function(response){
+			debugger;
+			endpoints.mobileHandler.getDashboard($scope.apiKey,$scope.userId,4,null,null, function(res){
+				debugger;
+			});
+		});
+		endpoints.mobileHandler.getDashboard($scope.apiKey, $scope.userId, 5, null, null, function(result){
+			if(result.result.success){
+				for(var i=0; i< result.result.result.Entries.length; i++){
+					$scope.allPolls.push(result.result.result.Entries[i]);
+				}
+			}
+			$scope.$apply();
+		});
+	});
+	
+	$scope.submitPoll = function(poll) {
+		var index = $scope.allPolls.indexOf(poll);
+		$scope.allPolls.splice(index, 1);
+		if($scope.allPolls.length <= 0){
+			setTimeout(function(){
+				$('.votingDone').animate({height: '0px', border: 'none', margin: '0', padding: '0'}, "500");
+			}, 3000);
+		}
+	}
+	
+});
+
+myApp.controller('navCtrl', function($scope, $cookieStore, $location, $localStorage){
+	$scope.logout = function() {
+		debugger;
+		$cookieStore.remove('loginDetails');
+		$cookieStore.remove('Logged_in');
+		delete $localStorage.loggedIn;
+		delete $localStorage.loginDetails;
+		$location.path('/');
+	}
+});
+
+myApp.controller('assignmentCtrl', function($scope, $location, $localStorage){
+	if(!$localStorage.loginDetails){
+		delete $localStorage.loggedIn;
+		$location.path('/');
+	}
+	$scope.initializeSlider = function() {
+		$('.bxslider').bxSlider({
+		  auto: true,
+		  autoHover: true
+		});
+	}
+	$scope.allAssignments = [];
+	var loginDetails = $localStorage.loginDetails;
+	$scope.apiKey = loginDetails[0].value;
+	$scope.userId = loginDetails[1].value;
+	$scope.panelistId = loginDetails[2].value;
+	$scope.registrationId = loginDetails[3].value;
+	
+	var endpoints = {};
+	endpoints.apiKey = $scope.apiKey;
+	// Creating new handler for APIs
+	endpoints.mobileHandler = new MobileHandler();
+	//Querying APi for response using endpoints
+	debugger;
+		endpoints.mobileHandler.getAssignments($scope.apiKey, $scope.userId, $scope.panelistId, function(result){
+			debugger;
+			if (result.result.success){
+				for (var i=0; i< result.result.result.length; i++) {
+					$scope.allAssignments.push(result.result.result[i]);
+					//$scope.allAssignments[i].options.instructions = html(result.result.result[i].options.instructions);
+				}
+			}
+			$scope.$apply();
+		});
+});
+
+myApp.controller('forumCtrl', function($scope){
+	debugger;
+});
+
+myApp.controller('messagesCtrl', function($scope, $cookieStore, $rootScope, $localStorage){
+	if(!$localStorage.loginDetails){
+		delete $localStorage.loggedIn;
+		$location.path('/');
+	}
+	$scope.messages = [];
+	$scope.tempArr = [];
+	
+	/* Getting all local Storage data for User Authentication */
+	var loginDetails = $localStorage.loginDetails;
+	$scope.apiKey = loginDetails[0].value;
+	$scope.userId = loginDetails[1].value;
+	$scope.panelistId = loginDetails[2].value;
+	$scope.registrationId = loginDetails[3].value;
+	var sectionId = 2;
+	
+	var endpoints = {};
+	endpoints.apiKey = $scope.apiKey;
+	// Creating new handler for APIs
+	endpoints.mobileHandler = new MobileHandler();
+	//Querying APi for response using endpoints
+	debugger;
+	endpoints.mobileHandler.getInbox($scope.apiKey, $scope.userId, 20, null, function(result){
+		if(result.result.result.Conversations){
+			for(var i=0; i<result.result.result.Conversations.length; i++){
+				$scope.messages.push(result.result.result.Conversations[i]);
+			}
+		}
+		$scope.$apply();
+	});
+	
+	// endpoints.mobileHandler.getInboxMessages($scope.apiKey, $scope.userId, "guid", null, null, function(callback){
+		// debugger;
+	// });
+});
+
+myApp.controller('profileCtrl', function($scope){
+	$scope.getHtml = function() {
+		return './navigation.html';
+	}
+	$scope.profilePage = 'setting-page-active';
+});
+
+myApp.controller('notificationCtrl', function($scope){
+	$scope.notificationPage = 'setting-page-active';
+});
+
+myApp.controller('resetpasswordCtrl', function($scope){
+	$scope.resetPassword = 'setting-page-active';
+}).directive('dynamic', function ($compile) {
+  return {
+    restrict: 'A',
+    replace: true,
+    link: function (scope, ele, attrs) {
+		ele.html(attrs.dynamic);
+        $compile(ele.contents())(scope);
+    }
+  };
+});
---
+++ js/main.js
@@ -0,0 +1,68 @@
+/*--slide out menus--*/
+(function($) {
+    $(document).ready(function() {
+      $.slidebars();
+    });
+  }) (jQuery);
+
+/*--assignment slider--*/
+$(document).ready(function(){
+  //if statement prevents errors on pages without bxslider
+});
+
+/*--tooltipster plugin--*/
+$(document).ready(function() {
+    $('.tooltip').tooltipster({
+        maxWidth: 300
+    });
+});
+
+//hides up/down votes upon clicking//
+$(".su1").click(function(){
+    $(".sd1").hide();
+});
+
+$(".sd1").click(function(){
+    $(".su1").hide();
+});
+
+$(".su2").click(function(){
+    $(".sd2").hide();
+});
+
+$(".sd2").click(function(){
+    $(".su2").hide();
+});
+
+$(".su3").click(function(){
+    $(".sd3").hide();
+});
+
+$(".sd3").click(function(){
+    $(".su3").hide();
+});
+
+$(".su4").click(function(){
+    $(".sd4").hide();
+});
+
+$(".sd4").click(function(){
+    $(".su4").hide();
+});
+
+$(".su5").click(function(){
+    $(".sd5").hide();
+});
+
+$(".sd5").click(function(){
+    $(".su5").hide();
+});
+
+$(".su6").click(function(){
+    $(".sd6").hide();
+});
+
+$(".sd6").click(function(){
+    $(".su6").hide();
+});
+
